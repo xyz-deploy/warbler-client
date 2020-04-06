8af7d54305914ca59b7c7d9341143e167b91a07a
xyz-deploy <63194477+xyz-deploy@users.noreply.github.com>
2020-04-06 14:47:34 +0530

    Update messages.js

--- src/store/actions/messages.js
+++ src/store/actions/messages.js
@@ -14,7 +14,7 @@ export const remove = id => ({
 
 export const removeMessage = (user_id, message_id) => {
   return dispatch => {
-    return apiCall("delete", `/api/users/${user_id}/messages/${message_id}`)
+    return apiCall("delete", `https://warbler-server2020.herokuapp.com/api/users/${user_id}/messages/${message_id}`)
       .then(() => dispatch(remove(message_id)))
       .catch(err => {
         addError(err.message);
@@ -24,7 +24,7 @@ export const removeMessage = (user_id, message_id) => {
 
 export const fetchMessages = () => {
   return dispatch => {
-    return apiCall("GET", "/api/messages")
+    return apiCall("GET", "https://warbler-server2020.herokuapp.com/api/messages")
       .then(res => {
         dispatch(loadMessages(res));
       })
@@ -37,7 +37,7 @@ export const fetchMessages = () => {
 export const postNewMessage = text => (dispatch, getState) => {
   let { currentUser } = getState();
   const id = currentUser.user.id;
-  return apiCall("post", `/api/users/${id}/messages`, { text })
+  return apiCall("post", `https://warbler-server2020.herokuapp.com/api/users/${id}/messages`, { text })
     .then(res => {})
     .catch(err => addError(err.message));
 };
